ed47a5df5bd7ba11ba3cedc9086ec446a54640ba
Hyuain <Hyuain97@163.com>
2019-12-11 23:37:42 +0800

    添加了动画效果

--- dist/main.a07bab4b.js
+++ dist/main.a07bab4b.js
@@ -117,7 +117,7 @@ parcelRequire = (function (modules, cache, entry, globalName) {
   }
 
   return newRequire;
-})({"main.js":[function(require,module,exports) {
+})({"epB2":[function(require,module,exports) {
 var $siteList = $('.site-list');
 var $lastLi = $siteList.find('li.last');
 var sites = JSON.parse(localStorage.getItem('sites'));
@@ -164,13 +164,13 @@ var render = function render() {
       render();
     });
     $(document).on('touchstart', function (e) {
-      $('.mobile-close-container', $li).css('display', 'none');
+      $('.mobile-close-container', $li).fadeOut(200);
     });
     $li.on({
       touchstart: function touchstart(e) {
         longClick = 0;
         timeOutEvent = setTimeout(function () {
-          $('.mobile-close-container', $li).css('display', 'block');
+          $('.mobile-close-container', $li).fadeIn(100);
           longClick = 1;
         }, 500);
         e.preventDefault();
@@ -196,10 +196,7 @@ render();
  * 设置添加网站事件
  */
 
-$(".add-button").on('click', function () {
-  $('#add-dialog-container').css('display', 'block');
-});
-$('#dialog-submit', '.add-dialog').on('click', function () {
+var submit = function submit() {
   var siteName = $("input[id='field-name']", '.add-dialog').val();
   var url = $("input[id='field-url']", '.add-dialog').val();
   $('input', '.add-dialog').val('');
@@ -223,13 +220,33 @@ $('#dialog-submit', '.add-dialog').on('click', function () {
     logoType: 'text',
     url: url
   });
-  $('#add-dialog-container').css('display', 'none');
+  $('#add-dialog-container').fadeOut(200);
   render();
   localStorage.setItem('sites', JSON.stringify(hashMap));
+};
+
+$('#add-button').click(function () {
+  $('#add-dialog-container').fadeIn(200);
+  $('#add-button').addClass('active');
+  $('.icon', '#add-button').addClass('active');
 });
-$('#dialog-close', '.add-dialog').on('click', function () {
-  $('#add-dialog-container').css('display', 'none');
+$('#dialog-submit').click(function () {
+  submit();
+});
+$('#field-url').keypress(function (e) {
+  var key = e.key;
+
+  if (key === 'Enter') {
+    submit();
+  }
+
+  e.stopPropagation();
+});
+$('#dialog-close').click(function () {
+  $('#add-dialog-container').fadeOut(200);
   $('input', '.add-dialog').val('');
+  $('#add-button').removeClass('active');
+  $('.icon', '#add-button').removeClass('active');
 });
 /**
  * 设置快捷键事件
@@ -237,10 +254,9 @@ $('#dialog-close', '.add-dialog').on('click', function () {
 
 $(document).on('keypress', function (e) {
   var key = e.key;
-  console.log(key);
   window.open(hashMap[key - 1].url, '_self');
 });
-$('input', '.search-from').on('keypress', function (e) {
+$('input', '.search-form').on('keypress', function (e) {
   e.stopPropagation();
 });
 $('input', '.add-dialog').on('keypress', function (e) {
@@ -251,214 +267,10 @@ $('#test').click(function () {
   location.reload();
 });
 $('input', '.search-form').focusin(function () {
-  $('.search-form').addClass('search-form-active');
+  $('.search-form').addClass('active');
 });
 $('input', '.search-form').focusout(function () {
-  $('.search-form').removeClass('search-form-active');
+  $('.search-form').removeClass('active');
 });
-},{}],"../../../AppData/Local/Yarn/Data/global/node_modules/parcel/src/builtins/hmr-runtime.js":[function(require,module,exports) {
-var global = arguments[3];
-var OVERLAY_ID = '__parcel__error__overlay__';
-var OldModule = module.bundle.Module;
-
-function Module(moduleName) {
-  OldModule.call(this, moduleName);
-  this.hot = {
-    data: module.bundle.hotData,
-    _acceptCallbacks: [],
-    _disposeCallbacks: [],
-    accept: function (fn) {
-      this._acceptCallbacks.push(fn || function () {});
-    },
-    dispose: function (fn) {
-      this._disposeCallbacks.push(fn);
-    }
-  };
-  module.bundle.hotData = null;
-}
-
-module.bundle.Module = Module;
-var checkedAssets, assetsToAccept;
-var parent = module.bundle.parent;
-
-if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
-  var hostname = "" || location.hostname;
-  var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "6363" + '/');
-
-  ws.onmessage = function (event) {
-    checkedAssets = {};
-    assetsToAccept = [];
-    var data = JSON.parse(event.data);
-
-    if (data.type === 'update') {
-      var handled = false;
-      data.assets.forEach(function (asset) {
-        if (!asset.isNew) {
-          var didAccept = hmrAcceptCheck(global.parcelRequire, asset.id);
-
-          if (didAccept) {
-            handled = true;
-          }
-        }
-      }); // Enable HMR for CSS by default.
-
-      handled = handled || data.assets.every(function (asset) {
-        return asset.type === 'css' && asset.generated.js;
-      });
-
-      if (handled) {
-        console.clear();
-        data.assets.forEach(function (asset) {
-          hmrApply(global.parcelRequire, asset);
-        });
-        assetsToAccept.forEach(function (v) {
-          hmrAcceptRun(v[0], v[1]);
-        });
-      } else if (location.reload) {
-        // `location` global exists in a web worker context but lacks `.reload()` function.
-        location.reload();
-      }
-    }
-
-    if (data.type === 'reload') {
-      ws.close();
-
-      ws.onclose = function () {
-        location.reload();
-      };
-    }
-
-    if (data.type === 'error-resolved') {
-      console.log('[parcel] ✨ Error resolved');
-      removeErrorOverlay();
-    }
-
-    if (data.type === 'error') {
-      console.error('[parcel] 🚨  ' + data.error.message + '\n' + data.error.stack);
-      removeErrorOverlay();
-      var overlay = createErrorOverlay(data);
-      document.body.appendChild(overlay);
-    }
-  };
-}
-
-function removeErrorOverlay() {
-  var overlay = document.getElementById(OVERLAY_ID);
-
-  if (overlay) {
-    overlay.remove();
-  }
-}
-
-function createErrorOverlay(data) {
-  var overlay = document.createElement('div');
-  overlay.id = OVERLAY_ID; // html encode message and stack trace
-
-  var message = document.createElement('div');
-  var stackTrace = document.createElement('pre');
-  message.innerText = data.error.message;
-  stackTrace.innerText = data.error.stack;
-  overlay.innerHTML = '<div style="background: black; font-size: 16px; color: white; position: fixed; height: 100%; width: 100%; top: 0px; left: 0px; padding: 30px; opacity: 0.85; font-family: Menlo, Consolas, monospace; z-index: 9999;">' + '<span style="background: red; padding: 2px 4px; border-radius: 2px;">ERROR</span>' + '<span style="top: 2px; margin-left: 5px; position: relative;">🚨</span>' + '<div style="font-size: 18px; font-weight: bold; margin-top: 20px;">' + message.innerHTML + '</div>' + '<pre>' + stackTrace.innerHTML + '</pre>' + '</div>';
-  return overlay;
-}
-
-function getParents(bundle, id) {
-  var modules = bundle.modules;
-
-  if (!modules) {
-    return [];
-  }
-
-  var parents = [];
-  var k, d, dep;
-
-  for (k in modules) {
-    for (d in modules[k][1]) {
-      dep = modules[k][1][d];
-
-      if (dep === id || Array.isArray(dep) && dep[dep.length - 1] === id) {
-        parents.push(k);
-      }
-    }
-  }
-
-  if (bundle.parent) {
-    parents = parents.concat(getParents(bundle.parent, id));
-  }
-
-  return parents;
-}
-
-function hmrApply(bundle, asset) {
-  var modules = bundle.modules;
-
-  if (!modules) {
-    return;
-  }
-
-  if (modules[asset.id] || !bundle.parent) {
-    var fn = new Function('require', 'module', 'exports', asset.generated.js);
-    asset.isNew = !modules[asset.id];
-    modules[asset.id] = [fn, asset.deps];
-  } else if (bundle.parent) {
-    hmrApply(bundle.parent, asset);
-  }
-}
-
-function hmrAcceptCheck(bundle, id) {
-  var modules = bundle.modules;
-
-  if (!modules) {
-    return;
-  }
-
-  if (!modules[id] && bundle.parent) {
-    return hmrAcceptCheck(bundle.parent, id);
-  }
-
-  if (checkedAssets[id]) {
-    return;
-  }
-
-  checkedAssets[id] = true;
-  var cached = bundle.cache[id];
-  assetsToAccept.push([bundle, id]);
-
-  if (cached && cached.hot && cached.hot._acceptCallbacks.length) {
-    return true;
-  }
-
-  return getParents(global.parcelRequire, id).some(function (id) {
-    return hmrAcceptCheck(global.parcelRequire, id);
-  });
-}
-
-function hmrAcceptRun(bundle, id) {
-  var cached = bundle.cache[id];
-  bundle.hotData = {};
-
-  if (cached) {
-    cached.hot.data = bundle.hotData;
-  }
-
-  if (cached && cached.hot && cached.hot._disposeCallbacks.length) {
-    cached.hot._disposeCallbacks.forEach(function (cb) {
-      cb(bundle.hotData);
-    });
-  }
-
-  delete bundle.cache[id];
-  bundle(id);
-  cached = bundle.cache[id];
-
-  if (cached && cached.hot && cached.hot._acceptCallbacks.length) {
-    cached.hot._acceptCallbacks.forEach(function (cb) {
-      cb();
-    });
-
-    return true;
-  }
-}
-},{}]},{},["../../../AppData/Local/Yarn/Data/global/node_modules/parcel/src/builtins/hmr-runtime.js","main.js"], null)
-//# sourceMappingURL=/main.1f19ae8e.js.map
+},{}]},{},["epB2"], null)
+//# sourceMappingURL=main.a07bab4b.js.map
--- src/main.js
+++ src/main.js
@@ -70,13 +70,13 @@ const render = () => {
             render()
         })
         $(document).on('touchstart', (e) => {
-            $('.mobile-close-container', $li).css('display', 'none')
+            $('.mobile-close-container', $li).fadeOut(200)
         })
         $li.on({
             touchstart: function (e) {
                 longClick = 0
                 timeOutEvent = setTimeout(function () {
-                    $('.mobile-close-container', $li).css('display', 'block')
+                    $('.mobile-close-container', $li).fadeIn(100)
                     longClick = 1
                 }, 500)
                 e.preventDefault()
@@ -102,42 +102,55 @@ render()
  * 设置添加网站事件
  */
 
-$(".add-button")
-    .on('click', () => {
-        $('#add-dialog-container').css('display', 'block')
+const submit = () => {
+    let siteName = $(`input[id='field-name']`, '.add-dialog').val()
+    let url = $(`input[id='field-url']`, '.add-dialog').val()
+    $('input', '.add-dialog').val('')
+    if (!siteName) {
+        siteName = simplifyUrl(url)
+    }
+    if (!url) {
+        alert('请务必输入网址哦')
+        return
+    }
+    if (url.indexOf('http') !== 0) {
+        url = 'https://' + url
+    }
+    hashMap.push({
+        name: siteName,
+        logo: siteName[0],
+        logoType: 'text',
+        url: url
     })
+    $('#add-dialog-container').fadeOut(200)
+    render()
+    localStorage.setItem('sites', JSON.stringify(hashMap))
+}
 
-$('#dialog-submit', '.add-dialog').
-    on('click', () => {
-        let siteName = $(`input[id='field-name']`, '.add-dialog').val()
-        let url = $(`input[id='field-url']`, '.add-dialog').val()
-        $('input', '.add-dialog').val('')
-        if (!siteName) {
-            siteName = simplifyUrl(url)
-        }
-        if (!url) {
-            alert('请务必输入网址哦')
-            return
-        }
-        if (url.indexOf('http') !== 0) {
-            url = 'https://' + url
-        }
-        hashMap.push({
-            name: siteName,
-            logo: siteName[0],
-            logoType: 'text',
-            url: url
-        })
-        $('#add-dialog-container').css('display', 'none')
-        render()
-        localStorage.setItem('sites', JSON.stringify(hashMap))
-    })
+$('#add-button').click(() => {
+    $('#add-dialog-container').fadeIn(200)
+    $('#add-button').addClass('active')
+    $('.icon','#add-button').addClass('active')
+})
 
-$('#dialog-close', '.add-dialog').
-    on('click', () => {
-        $('#add-dialog-container').css('display', 'none')
-        $('input', '.add-dialog').val('')
-    })
+$('#dialog-submit').click(() => {
+    submit()
+})
+
+$('#field-url').keypress((e) => {
+    const { key } = e
+    if (key === 'Enter') {
+        submit()
+    }
+    e.stopPropagation()
+})
+
+$('#dialog-close').click(() => {
+    $('#add-dialog-container').fadeOut(200)
+    $('input', '.add-dialog').val('')
+    $('#add-button').removeClass('active')
+    $('.icon','#add-button').removeClass('active')
+})
 
 /**
  * 设置快捷键事件
@@ -145,11 +158,10 @@ $('#dialog-close', '.add-dialog').
 
 $(document).on('keypress', (e) => {
     const { key } = e
-    console.log(key)
     window.open(hashMap[key - 1].url, '_self')
 })
 
-$('input', '.search-from').on('keypress', (e) => {
+$('input', '.search-form').on('keypress', (e) => {
     e.stopPropagation()
 })
 
@@ -162,10 +174,10 @@ $('#test').click(() => {
     location.reload()
 })
 
-$('input','.search-form').focusin(()=>{
-    $('.search-form').addClass('search-form-active')
+$('input', '.search-form').focusin(() => {
+    $('.search-form').addClass('active')
 })
 
-$('input','.search-form').focusout(()=>{
-    $('.search-form').removeClass('search-form-active')
+$('input', '.search-form').focusout(() => {
+    $('.search-form').removeClass('active')
 })
